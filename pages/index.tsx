e4372a08fa1f21f9233f74eb12d78ff5d78c8c6e
Shubham Patil <shubhampatilsd@gmail.com>
2025-05-22 02:48:45 -0700

    yayyy

--- pages/index.tsx
+++ pages/index.tsx
@@ -116,7 +116,7 @@ export default function Home() {
         <div className="w-full relative">
           <div className="fade-in-animation bg-[url('/sky.jpg')] bg-cover w-full md:h-[35vh] h-[20vh]"></div>
 
-          <div className="px-7 md:px-16 pt-10 md:pt-18 fade-in-animation">
+          <div className="mx-auto md:max-w-[90vw] px-7 md:px-16 pt-10 md:pt-18 fade-in-animation">
             <div className=" ">
               <h1 className="text-4xl md:text-6xl from-gray-800 to-gray-700  bg-clip-text text-transparent bg-gradient-to-t z-10 relative font-medium">
                 <span
@@ -149,7 +149,7 @@ export default function Home() {
               </p>
             </div>
 
-            <div className="md:max-w-[70vw] grid grid-cols-1 md:grid-cols-3 w-full mt-10 md:mt-18 gap-[10vw]">
+            <div className="grid grid-cols-1 md:grid-cols-3 w-full mt-10 md:mt-18 gap-[10vw]">
               <div className=" text-slate-500 space-y-5 max-w-xs">
                 <p>
                   Oftentimes, we lose ourselves in the pursuit of attaining mere
